84dac1a53d78756a5cb5d8614e2a95315629e348
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-08-09 09:43:06 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var schedule = require('node-schedule');
+var env      = require('./env');
+var spawn    = require('child_process').spawn;
+
+var j = schedule.scheduleJob('0 30 * * * *', function() {
+  spawn('open', env.SLACK_URLS());
+});
