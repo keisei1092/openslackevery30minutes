76c0571c33e47575506113045386630106a04c54
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-08-10 00:19:46 +0900

    9時から20時に制限

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var schedule = require('node-schedule');
 var env      = require('./env');
 var spawn    = require('child_process').spawn;
 
-var j = schedule.scheduleJob('0 0,30 * * * *', function() {
+var j = schedule.scheduleJob('0 0,30 9-20 * * *', function() {
   spawn('open', env.SLACK_URLS());
 });
